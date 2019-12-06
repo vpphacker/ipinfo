ea632c3d7b69b8c8cbbd33f18cb6eb36d2938cb8
VPPHacker <48642846+vpphacker@users.noreply.github.com>
2019-12-07 01:41:28 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 apt install python -y
 apt install python2 -y
 apt install ruby -y
+gem install lolcat
 echo '
  ==============================================
 |         Now Type python3 ipinfo.py           |
